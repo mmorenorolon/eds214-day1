fa9e7301117c47450cbcb548896ff8a125b1a5eb
mmorenorolon <mmorenorolon@ucsb.edu>
2025-08-26 22:54:22 +0000

    rm.na = TRUE in loop calculation

--- scratch/data-analysis-datapasta.R
+++ scratch/data-analysis-datapasta.R
@@ -29,7 +29,8 @@ parameters <- c("NO3-N", "K", "Mg", "Ca", "NH4-N")
       #be able to group into the parameters more easily, and clean names
 data_clean <- data_joined %>% 
   select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`) %>% 
-  pivot_longer(cols = all_of(parameters), names_to = "parameter", values_to = "concentration") %>% 
+  pivot_longer(cols = all_of(parameters), names_to = "parameter", 
+               values_to = "concentration") %>% 
   mutate(week_number = isoweek(Sample_Date),
          year = year(Sample_Date),
          #I'm trying to see if making a year and week number column
@@ -55,11 +56,11 @@ i <- 1
 
 while (i < 1:nrow(data_weekly) - window_size + 1) {
   
-  #Attempting to calculaye the moving average
+  #Attempting to calculate the moving average
   #the elements in the dataframe to call the interval we want
-  window_avg <- round(sum(data_weekly$weekly_mean[i:i + window_size]) / 
-                            window_size
-                          , 2)
+  window_avg <- round(sum(data_weekly$weekly_mean[i:i + window_size], 
+                          na.rm = TRUE) / 
+                            window_size, 2)
   
   #store values in the empty vector
   moving_avg[i] <- interval_avg
