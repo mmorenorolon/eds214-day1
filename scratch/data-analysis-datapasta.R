925a2ac8b2536cd2aabc79ea730ef9fced57cca7
mmorenorolon <mmorenorolon@ucsb.edu>
2025-08-26 21:37:10 +0000

    updating loop

--- scratch/data-analysis-datapasta.R
+++ scratch/data-analysis-datapasta.R
@@ -26,7 +26,7 @@ parameters <- c("NO3-N", "K", "Mg", "Ca", "NH4-N")
 
 
       #use joined data to subset into desired parameters, use pivot_longer() to
-      #be able to be able to group into the parameters more easily, and clean names
+      #be able to group into the parameters more easily, and clean names
 data_clean <- data_joined %>% 
   select(Sample_ID, Sample_Date, K, Mg, Ca, `NH4-N`, `NO3-N`) %>% 
   pivot_longer(cols = all_of(parameters), names_to = "parameter", values_to = "concentration") %>% 
@@ -41,37 +41,33 @@ data_clean <- data_joined %>%
 #Calculate the weekly average of each parameter by stream
 data_weekly <- data_clean %>% 
   group_by(sample_id, parameter, sample_week) %>% 
-  summarise(weekly_mean = mean(concentration, na.rm = TRUE)) %>% 
+  summarise(weekly_mean = round(mean(concentration, na.rm = TRUE), 2)) %>% 
   ungroup()
 
 #approach 1 to calculating moving average:
 
-  #creating an empty vector to store resulting moving averages
-moving_avg <- vector(mode = "numeric", length = nrow(data_weekly))
-
+  #create an empty column to store in 
   #defining moving average width f 9 weeks
-ma_width <- 9
+window_size <- 9
 
 i <- 1
   # a loop to iterate through every moving 9 weeks.
-moving_fun <- function(x, w, mean)
-for (i in 1:nrow(length(data_weekly)) - ma_width +1) {
+
+while (i < 1:nrow(data_weekly) - window_size + 1) {
   
-  #Attempting to store the elements in the dataframe to call the interval we want
-  interval = data_weekly$weekly_mean[i : (i + ma_width - 1)]
-  
-  interval_avg = mean()
+  #Attempting to calculaye the moving average
+  #the elements in the dataframe to call the interval we want
+  window_avg <- round(sum(data_weekly$weekly_mean[i:i + window_size]) / 
+                            window_size
+                          , 2)
   
   #store values in the empty vector
   moving_avg[i] <- interval_avg
   
+  #return output in dataframe
+  print()
   # Shift the starting value in interval to the right by one position
   i + 1
 } 
-  
-
-data_weekly %>% 
-  group_by(sample_id, parameter, sample_week) %>% 
-  summarise(cum_sum = cumsum(sample_week))
 
    
